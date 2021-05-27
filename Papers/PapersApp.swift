c364d40aa3184a5c991c00434cfaabeae2283db4
Purav <manot.purav@gmail.com>
2021-05-27 20:25:11 +0530

    Initial Commit

---
+++ Papers/PapersApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapersApp.swift
+//  Papers
+//
+//  Created by Purav Manot on 27/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PapersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
