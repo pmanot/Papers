86b90ec59dbe53367b1cf830fe439f53aa7c732c
Purav <manot.purav@gmail.com>
2021-06-29 03:55:22 +0530

    Refactor code and clean UI

--- Targets/Papers/Files/Views/Questions/QuestionList.swift
+++ Targets/Papers/Files/Views/Questions/QuestionList.swift
@@ -14,15 +14,43 @@ struct QuestionList: View {
     
     var body: some View {
         List(paper.questions) { question in
-            NavigationLink(destination: QuestionView(question)) {
-                Text(String(question.index))
-            }
+            Row(question)
         }
+        .navigationTitle("Questions")
     }
 }
 
 struct QuestionList_Previews: PreviewProvider {
     static var previews: some View {
         QuestionList(Paper.example)
+    }
+}
+
+
+extension QuestionList{
+    struct Row: View {
+        var question: Question
+        
+        init(_ question: Question) {
+            self.question = question
+        }
+        
+        var body: some View {
+            NavigationLink(destination: QuestionView(question)) {
+                HStack {
+                    Group {
+                        Text(String(question.index))
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.heavy)
+                            .frame(width: 20)
+                            .padding()
+                        Text("\(question.pages.count) page" + (question.pages.count > 1 ? "s" : ""))
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                            .frame(width: 60, alignment: .leading)
+                    }
+                }
+            }
+        }
     }
 }
