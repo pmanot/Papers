d06a8ad0a511296d1a76b65201149ae599b6c620
Purav <manot.purav@gmail.com>
2021-06-29 03:54:18 +0530

    Add MarkScheme

--- Targets/Papers/Files/Models/Paper.swift
+++ Targets/Papers/Files/Models/Paper.swift
@@ -11,6 +11,7 @@ struct Paper: Identifiable, Hashable {
     let url: URL
     let year: Int
     let pdf: PDFDocument
+    let markscheme: MarkScheme?
     var questions: [Question] = []
     
     var subject: Subject {
@@ -38,6 +39,13 @@ struct Paper: Identifiable, Hashable {
         url = URL(fileURLWithPath: Bundle.main.path(forResource: paperCode, ofType: "pdf")!)
         pdf = PDFDocument(url: url)!
         year = Int("20\(paperCode.dropFirst(12))") ?? 0
+        
+        let msCode = paperCode.replacingOccurrences(of: "qp", with: "ms")
+        if Bundle.main.path(forResource: msCode, ofType: "pdf") != nil {
+            markscheme = MarkScheme(msCode)
+        } else {
+            markscheme = nil
+        }
         
         extractQuestions()
     }
@@ -74,8 +82,48 @@ struct Paper: Identifiable, Hashable {
             }
         }
     }
+    
+    
+    
 }
 
 extension Paper {
     static let example = Paper("9702_s19_qp_21")
+}
+
+struct MarkScheme: Identifiable, Hashable {
+    let id = UUID()
+    let msCode: String
+    let url: URL
+    let year: Int
+    let pdf: PDFDocument
+    var answers: [Answer] = []
+    
+    init(_ msCode: String) {
+        self.msCode = msCode
+        
+        url = URL(fileURLWithPath: Bundle.main.path(forResource: msCode, ofType: "pdf")!)
+        pdf = PDFDocument(url: url)!
+        year = Int("20\(msCode.dropFirst(12))") ?? 0
+        
+        extractAnswers()
+    }
+    
+    mutating func extractAnswers() {
+        var answerPages: [Int: [Int]] = [:]
+        var answerNumber: Int = 1
+        
+        for pageNumber in 3..<pdf.pageCount {
+            answerPages[answerNumber] = [pageNumber]
+            answerNumber += 1
+        }
+        
+        for index in answerPages.keys {
+            if answerPages[index] != [] {
+                answers.append(Answer(markscheme: self, page: answerPages[index]!, index: index))
+                answers.sort(by: {$0.index < $1.index})
+            }
+        }
+    }
+    
 }
