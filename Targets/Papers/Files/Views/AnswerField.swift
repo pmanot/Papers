1fdace2fcdfc475fc206c61375a63163b1b35178
Purav <manot.purav@gmail.com>
2021-07-02 01:27:23 +0530

    Add Multiple Answer & Index support

--- Targets/Papers/Files/Views/AnswerField.swift
+++ Targets/Papers/Files/Views/AnswerField.swift
@@ -5,12 +5,9 @@
 import SwiftUI
 
 struct AnswerField: View {
-    @State private var text: String = ""
     @State private var answers: [Answer] = [Answer(paper: Paper.example)]
-    @State private var letterChosen: Bool = false
     var question: Question
     @State var index: QuestionIndex = QuestionIndex()
-    @Namespace private var animation
     
     init(_ question: Question){
         UITextView.appearance().backgroundColor = .clear
@@ -18,28 +15,79 @@ struct AnswerField: View {
     }
     
     var body: some View {
-        ZStack(alignment: .topLeading) {
-            TextEditor(text: $text)
-                .foregroundColor(.primary)
-                .padding(.top, 52)
-                .padding(.horizontal, 6)
-                .font(.body)
+        ZStack(alignment: .bottom) {
+            Capsule()
+                .frame(width: answers.count > 1 ? 20*CGFloat((answers.count)) : 0, height: 18.5)
+                .padding()
                 .foregroundColor(.black)
-                .background(Color.primary.clipShape(RoundedRectangle(cornerRadius: 12)).colorInvert())
-                .opacity(0.8)
-                .overlay(RoundedRectangle(cornerRadius: 12).strokeBorder(antialiased: true).foregroundColor(.primary))
-            
-            HStack {
+            TabView {
+                ForEach(answers, id: \.id) { answer in
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .bottom) {
+                            IndexPicker(index: $answers[answers.firstIndex(of: answer)!].index, question: question)
+                            
+                            ButtonSymbol("plus.square.fill"){
+                                withAnimation {
+                                    answers.append(Answer(paper: Paper.example, index: QuestionIndex(letter: QuestionIndex.letters[QuestionIndex.letters.firstIndex(of: answers.last!.index.letter!)! + 1])))
+                                }
+                            }
+                            .font(.title)
+                            .frame(width: 26, height: 26)
+                            .foregroundColor(.green)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .strokeBorder(antialiased: true)
+                            )
+                        }
+                        .padding(.vertical, 5)
+                        
+                        TextEditor(text: $answers[answers.firstIndex(of: answer)!].text)
+                            .foregroundColor(.primary)
+                            .padding(.horizontal, 6)
+                            .font(.body)
+                            .background(Color.primary.clipShape(RoundedRectangle(cornerRadius: 12)).colorInvert())
+                            .opacity(0.8)
+                            .overlay(RoundedRectangle(cornerRadius: 12).strokeBorder(antialiased: true).foregroundColor(.primary))
+                            .padding([.bottom, .horizontal, .top], 5)
+                    }
+                }
+                .animation(.easeIn)
+            }
+            .tabViewStyle(PageTabViewStyle())
+        }
+        .animation(.spring())
+    }
+}
+
+struct AnswerField_Previews: PreviewProvider {
+    static var previews: some View {
+        AnswerField(Paper.example.questions[0])
+    }
+}
+
+extension AnswerField {
+    struct IndexPicker: View {
+        @Namespace private var animation
+        @Binding var index: QuestionIndex
+        let question: Question
+        
+        var body: some View {
+            HStack(alignment: .bottom) {
                 Text(String(question.index))
                     .font(.largeTitle)
                     .fontWeight(.black)
                     .overlay(
-                        RoundedRectangle(cornerRadius: 10)
+                        RoundedRectangle(cornerRadius: 8)
                             .strokeBorder(antialiased: true)
-                            .frame(width: 50, height: 50)
+                            .frame(width: 45, height: 45)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .strokeBorder(antialiased: true)
+                                    .frame(width: 50, height: 50)
+                            )
                     )
                     .frame(width: 50, height: 50)
-                    .padding([.leading, .vertical], 5)
+                    .padding(.leading, 5)
                 Group {
                     if index.letter == nil {
                         Picker("", selection: $index){
@@ -55,7 +103,7 @@ struct AnswerField: View {
                             Text(index.letter ?? "")
                                 .bold()
                                 .frame(width: 30, height: 30)
-                                .background(Color.secondary.clipShape(RoundedRectangle(cornerRadius: 10)).opacity(0.5))
+                                .background(Color.secondary.clipShape(RoundedRectangle(cornerRadius: 10)).opacity(0.8))
                         }
                         .buttonStyle(PlainButtonStyle())
                     }
@@ -65,17 +113,7 @@ struct AnswerField: View {
                         .strokeBorder(antialiased: true)
                 )
                 .matchedGeometryEffect(id: "chooseIndex", in: animation)
-                .padding(.vertical, 10)
             }
         }
-        .animation(.spring())
-        .padding()
     }
 }
-
-struct AnswerField_Previews: PreviewProvider {
-    static var previews: some View {
-        AnswerField(Paper.example.questions[0])
-    }
-}
-
