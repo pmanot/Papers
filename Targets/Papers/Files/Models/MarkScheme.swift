d6903969710ef6af85477d1c60a7661ae0a75428
Purav <manot.purav@gmail.com>
2021-07-03 16:02:20 +0530

    Create MarkScheme

--- Targets/Papers/Files/Models/MarkScheme.swift
+++ Targets/Papers/Files/Models/MarkScheme.swift
@@ -1,8 +1,45 @@
 //
-//  MarkScheme.swift
-//  Papers
-//
-//  Created by Purav Manot on 29/06/21.
+// Copyright (c) Purav Manot
 //
 
 import Foundation
+import PDFKit
+
+struct MarkScheme: Identifiable, Hashable {
+    let id = UUID()
+    let code: String
+    let url: URL
+    let year: Int
+    let pdf: PDFDocument
+    var answers: [AnswerSheet] = []
+    
+    init(_ msCode: String) {
+        self.code = msCode
+        url = URL(fileURLWithPath: Bundle.main.path(forResource: code, ofType: "pdf")!)
+        pdf = PDFDocument(url: url)!
+        year = Int("20\(code.dropFirst(12))") ?? 0
+        
+        extractAnswers()
+    }
+    
+    mutating func extractAnswers() {
+        var answerPages: [Int: [Int]] = [:]
+        var answerNumber: Int = 1
+        
+        for pageNumber in 3..<pdf.pageCount {
+            answerPages[answerNumber] = [pageNumber]
+            answerNumber += 1
+        }
+        
+        for index in answerPages.keys {
+            if answerPages[index] != [] {
+                answers.append(AnswerSheet(markscheme: self, page: answerPages[index]!, index: index))
+                answers.sort(by: {$0.index < $1.index})
+            }
+        }
+    }
+}
+
+extension MarkScheme {
+    static let example = MarkScheme("9702_s19_ms_21")
+}
