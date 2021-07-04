cf63c3d890f968bbcdfb4e1c3de0321f0f7c4436
Purav <manot.purav@gmail.com>
2021-07-04 16:45:04 +0530

    Create DocumentDirectory

---
+++ Targets/Papers/Files/Models/DocumentDirectory.swift
@@ -0,0 +1,37 @@
+//
+// Copyright (c) Purav Manot
+//
+
+import Foundation
+
+struct DocumentDirectory {
+    var url: URL {
+        getDocumentsDirectory()
+    }
+    
+    func write(_ string: String, to location: String){
+        let documentURL = url.appendingPathComponent("\(location).txt")
+        do {
+            try string.write(to: documentURL, atomically: true, encoding: .utf8)
+        } catch {
+            print("Write Error: ", error.localizedDescription)
+        }
+    }
+    
+    func read(from location: String) -> String {
+        do {
+            let str = try String(contentsOf: url.appendingPathComponent("\(location).txt"))
+            return str
+        } catch {
+            print("Read Error: ", error.localizedDescription)
+        }
+        return ""
+    }
+    
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
+
