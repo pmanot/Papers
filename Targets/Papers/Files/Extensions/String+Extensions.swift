8d85276ee111ffffae8144bcc8ac7252400b85a9
Purav <manot.purav@gmail.com>
2021-07-11 13:52:44 +0530

    Create wordCount() extension

---
+++ Targets/Papers/Files/Extensions/String+Extensions.swift
@@ -0,0 +1,21 @@
+//
+// Copyright (c) Purav Manot
+//
+
+import Foundation
+
+extension String {
+    func wordCount() -> Int {
+        var count: Int = 0
+        var space: Bool = true
+        for char in self.appending(" ") {
+            if char == " " && space == false {
+                count += 1
+                space = true
+            } else if char != " " {
+                space = false
+            }
+        }
+        return count
+    }
+}
