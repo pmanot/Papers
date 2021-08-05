6a817b415b2f55f47fad39ec303dbc3273ca66cb
Purav Manot <manot.purav@gmail.com>
2021-08-05 11:30:54 -0700

    Create CambridgePaperVariant

---
+++ Targets/Papers/Files/Models/Cambridge/CambridgePaperVariant.swift
@@ -0,0 +1,11 @@
+//
+// Copyright (c) Purav Manot
+//
+
+import Foundation
+
+public enum CambridgePaperVariant: String, Codable {
+    case variant1 = "Variant 1"
+    case variant2 = "Variant 2"
+    case variant3 = "Variant 3"
+}
