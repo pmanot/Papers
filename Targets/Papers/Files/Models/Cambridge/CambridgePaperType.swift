b7339f79a7b37b548d82bab8e00061d9d26d0e8f
Purav Manot <manot.purav@gmail.com>
2021-08-05 11:30:41 -0700

    Create CambridgePaperType

---
+++ Targets/Papers/Files/Models/Cambridge/CambridgePaperType.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) Purav Manot
+//
+
+import Foundation
+
+public enum CambridgePaperType: String, Codable {
+    case paper1 = "Paper 1"
+    case paper2 = "Paper 2"
+    case paper3 = "Paper 3"
+    case paper4 = "Paper 4"
+    case paper5 = "Paper 5"
+    case paper6 = "Paper 6"
+}
