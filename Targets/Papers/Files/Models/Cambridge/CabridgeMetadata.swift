c496d4f39aa2586f6cc0a4837d7bc1621069dd70
Purav Manot <manot.purav@gmail.com>
2021-09-01 23:39:20 +0530

    Create CambridgePaperType

--- Targets/Papers/Files/Models/Cambridge/CabridgeMetadata.swift
+++ Targets/Papers/Files/Models/Cambridge/CabridgeMetadata.swift
@@ -57,9 +57,62 @@ struct CambridgePaperCode: Hashable, Codable {
     }
 }
 
-enum CambridgePaperCodeType: Int, Codable {
-    case markscheme = 0
-    case questionPaper = 1
-    case datasheet = 2
-    case other = 3
+enum CambridgePaperType: Hashable {
+    case markscheme(number: CambridgePaperNumber, variant: CambridgePaperVariant)
+    case questionPaper(number: CambridgePaperNumber, variant: CambridgePaperVariant)
+    case datasheet
+    case other
+}
+
+extension CambridgePaperType {
+    private struct PaperTypeIdentifier: Codable {
+        let number: CambridgePaperNumber
+        let variant: CambridgePaperVariant
+    }
+}
+
+extension CambridgePaperType: Codable {
+    
+    private enum CodingKeys: CodingKey {
+        case base, typeIdentifier
+    }
+    
+    private enum Base: String, Codable {
+        case questionPaper, markscheme, datasheet, other
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .datasheet:
+            try container.encode(Base.datasheet, forKey: .base)
+        case .other:
+            try container.encode(Base.other, forKey: .base)
+        case .questionPaper(let number, let variant):
+            try container.encode(Base.questionPaper, forKey: .base)
+            try container.encode(PaperTypeIdentifier(number: number, variant: variant), forKey: .typeIdentifier)
+        case .markscheme(let number, let variant):
+            try container.encode(Base.markscheme, forKey: .base)
+            try container.encode(PaperTypeIdentifier(number: number, variant: variant), forKey: .typeIdentifier)
+        }
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let base = try container.decode(Base.self, forKey: .base)
+        
+        switch base {
+        case .datasheet:
+            self = .datasheet
+        case .other:
+            self = .other
+        case .questionPaper:
+            let typeIdentifier = try container.decode(PaperTypeIdentifier.self, forKey: .typeIdentifier)
+            self = .questionPaper(number: typeIdentifier.number, variant: typeIdentifier.variant)
+        case .markscheme:
+            let typeIdentifier = try container.decode(PaperTypeIdentifier.self, forKey: .typeIdentifier)
+            self = .markscheme(number: typeIdentifier.number, variant: typeIdentifier.variant)
+        }
+    }
+    
 }
