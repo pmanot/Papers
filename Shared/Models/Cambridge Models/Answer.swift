18bd44584ba13b6d3f820914157af5c296a851b3
Purav Manot <manot.purav@gmail.com>
2021-12-01 16:12:12 +0530

    Add example answers

--- Shared/Models/Cambridge Models/Answer.swift
+++ Shared/Models/Cambridge Models/Answer.swift
@@ -99,3 +99,8 @@ enum MCQSelection: String, Codable, Hashable {
     case D = "D"
     case none = "N"
 }
+
+extension Array where Element == Answer {
+    static var exampleAnswers: [Answer] = (1...40).map {Answer(index: QuestionIndex($0), value: .multipleChoice(choice: .A))}
+    static var exampleCorrectAnswers: [Answer] = (1...10).map {Answer(index: QuestionIndex($0), value: .multipleChoice(choice: .A))} + (11...35).map {Answer(index: QuestionIndex($0), value: .multipleChoice(choice: .C))} + (36...40).map {Answer(index: QuestionIndex($0), value: .multipleChoice(choice: .B))}
+}
