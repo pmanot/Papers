b641185f585b3e4afc53d58cf51afa6acab4649d
Purav Manot <manot.purav@gmail.com>
2021-12-08 13:12:16 +0530

    Rewrite & refactor code

--- Shared/Views/Papers/SolvedPaperCollectionView.swift
+++ Shared/Views/Papers/SolvedPaperCollectionView.swift
@@ -1,52 +1,75 @@
 //
 // Copyright (c) Purav Manot
 //
+
 import SwiftUI
 
 struct SolvedPaperCollectionView: View {
-    var solvedPapers: [SolvedPaper] = [SolvedPaper.example, SolvedPaper.example, SolvedPaper.example, SolvedPaper.example]
-    @State private var expanded: Bool = false
+    var solvedPapers: [SolvedPaper]
+    @Binding var expanded: Bool
+    @Namespace private var paperCards
+    
+    init(solvedPapers: [SolvedPaper], expanded: Binding<Bool> = .constant(false)){
+        self.solvedPapers = solvedPapers
+        self._expanded = expanded
+    }
+    
     var body: some View {
-        GeometryReader { screen in
-            ScrollView(.vertical) {
-                ZStack {
-                    ForEach(0..<solvedPapers.count){ i in
+        ZStack {
+            switch expanded {
+                case true:
+                    VStack {
+                        
+                        ForEach(0..<solvedPapers.count, id: \.self){ i in
+                            MCQSolvedView(solvedPapers[i])
+                                .miniSolvedPaperWidget
+                                .frame(width: 400)
+                                .matchedGeometryEffect(id: i, in: paperCards)
+                                .zIndex(Double(solvedPapers.count - i))
+                                .padding(5)
+                        }
+                    }
+                    .zIndex(10)
+                case false:
+                    ForEach(0..<solvedPapers.count, id: \.self){ i in
                         MCQSolvedView(solvedPapers[i])
                             .miniSolvedPaperWidget
-                            .zIndex(Double(-i))
+                            .frame(width: 400)
                             .rotationEffect(rotationValue(i))
-                            .position(positionValue(i, screen))
+                            .matchedGeometryEffect(id: i, in: paperCards)
+                            .zIndex(Double(solvedPapers.count - i))
                     }
-                }
-                .frame(height: expanded ? 200*CGFloat(solvedPapers.count) + 100 : 400)
-                .onTapGesture {
-                    withAnimation(.spring(dampingFraction: 0.8)) {
-                        expanded.toggle()
-                    }
-                }
             }
         }
-        .frame(width: 400, height: 550)
+        .onTapGesture {
+            expand()
+        }
     }
 }
 
 extension SolvedPaperCollectionView {
     private func rotationValue(_ i: Int) -> Angle {
-        return expanded ? .degrees(0) : .degrees(-min(i, 3)*4)
+        expanded ? .degrees(0) : .degrees(-min(i, 3)*4)
     }
     
-    private func positionValue(_ i: Int, _ screen: GeometryProxy) -> CGPoint {
-        if expanded {
-        return CGPoint(x: screen.center.x, y: screen.center.y + CGFloat(i*200) - CGFloat(100*min(solvedPapers.count, 3)/2))
-        } else {
-            return CGPoint(x: screen.center.x - CGFloat(i*5), y: screen.center.y + CGFloat(i*2))
+    private func calculatedOffset(_ i: Int, screen: GeometryProxy) -> CGFloat {
+        expanded ? CGFloat(100) : screen.center.y - CGFloat(i)*CGFloat(solvedPapers.count - 1)*25
+    }
+    
+    private func expand() {
+        withAnimation(.spring(dampingFraction: 0.8)) {
+            expanded.toggle()
         }
+    }
+    
+    private func calculatedHeight() -> CGFloat {
+        expanded ? CGFloat(200*solvedPapers.count) : CGFloat(300)
     }
 }
 
 struct SolvedPaperCollectionView_Previews: PreviewProvider {
     static var previews: some View {
-        SolvedPaperCollectionView()
+        SolvedPaperCollectionView(solvedPapers: [SolvedPaper.example, SolvedPaper.example, SolvedPaper.example, SolvedPaper.example])
     }
 }
 
