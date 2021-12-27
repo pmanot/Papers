474e4f85a3ebf57b6d3ca81e5db8518632920891
Purav Manot <manot.purav@gmail.com>
2021-12-27 19:45:20 +0530

    Add exampleDeck

--- Shared/Models/UI Elements/Card.swift
+++ Shared/Models/UI Elements/Card.swift
@@ -12,4 +12,5 @@ struct Card: Identifiable, Hashable {
 
 extension Card {
     static var example: Card = Card(prompt: "What are photons?", answer: "Discreet packets of energy")
+    static var exampleDeck: [Card] = [Card(prompt: "What are photons?", answer: "Discreet packets of energy"), Card(prompt: "Define force", answer: "The rate of change of momentum"), Card(prompt: "Define velocity", answer: "The rate of change of displacement"), Card(prompt: "0 Celsius (C) in Kelvin (K)", answer: "273 Kelvin (K)")]
 }
