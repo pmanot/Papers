f5a7d6c7edc1fa3f0c1c890d6a8b890a1ef09f45
Purav Manot <manot.purav@gmail.com>
2021-12-29 17:16:10 +0530

    Add extra subject cases

--- Shared/Models/Cambridge Models/CambridgeSubject.swift
+++ Shared/Models/Cambridge Models/CambridgeSubject.swift
@@ -16,10 +16,14 @@ enum CambridgeSubject: String, Codable, Hashable {
 extension CambridgeSubject {
     init(paperCode: String){
         switch paperCode.dropLast(10) {
+            case "9700":
+                self = .biology
             case "9701":
                 self = .chemistry
             case "9702":
                 self = .physics
+            case "9093":
+                self = .english
             default:
                 self = .other
         }
