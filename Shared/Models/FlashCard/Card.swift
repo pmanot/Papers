a5db0a5d74957d65eddfe27c9ef30954426eea17
Purav Manot <manot.purav@gmail.com>
2021-12-31 12:08:46 +0530

    Refactor & add properties and methods

--- Shared/Models/FlashCard/Card.swift
+++ Shared/Models/FlashCard/Card.swift
@@ -6,11 +6,23 @@ import Foundation
 
 struct Card: Identifiable, Hashable {
     let id = UUID()
-    let prompt: String
-    let answer: String
+    var prompt: String
+    var answer: String
+    var keywords: [String] = []
 }
 
 extension Card {
     static var example: Card = Card(prompt: "What are photons?", answer: "Discreet packets of energy")
+    static var empty: Card = Card(prompt: "", answer: "")
     static var exampleDeck: [Card] = [Card(prompt: "What are photons?", answer: "Discreet packets of energy"), Card(prompt: "Define force", answer: "The rate of change of momentum"), Card(prompt: "Define velocity", answer: "The rate of change of displacement"), Card(prompt: "0 Celsius (C) in Kelvin (K)", answer: "273 Kelvin (K)")]
+    
+    func generate() -> Card {
+        Card(prompt: self.prompt, answer: self.answer)
+    }
+    
+    func isEmpty() -> Bool {
+        prompt == "" && answer == ""
+    }
 }
+
+
