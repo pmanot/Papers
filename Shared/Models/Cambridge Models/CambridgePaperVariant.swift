a9e6cc65c90171904f0dedd0e018d0158eafbc65
Purav Manot <manot.purav@gmail.com>
2021-12-31 12:03:06 +0530

    Add extra cases to CambridgePaperVariant

--- Shared/Models/Cambridge Models/CambridgePaperVariant.swift
+++ Shared/Models/Cambridge Models/CambridgePaperVariant.swift
@@ -8,6 +8,11 @@ public enum CambridgePaperVariant: Int, Codable, Hashable {
     case variant1 = 1
     case variant2 = 2
     case variant3 = 3
+    case variant4 = 4
+    case variant5 = 5
+    case variant6 = 6
+    case variant7 = 7
+    case variant8 = 8
 }
 
 extension CambridgePaperVariant {
