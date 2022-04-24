6e647e0a075e55af0cf91324993af6ca810997f8
Purav Manot <manot.purav@gmail.com>
2022-04-25 00:24:05 +0530

    Remove contents from CambridgePaperPage

--- Shared/Models/Cambridge/Enums/CambridgePaperPage.swift
+++ Shared/Models/Cambridge/Enums/CambridgePaperPage.swift
@@ -2,11 +2,17 @@
 // Copyright (c) Purav Manot
 //
 
-import Foundation
-import SwiftUI
+import FoundationX
 import PDFKit
+import Swallow
+import SwiftUIX
 
 struct CambridgePaperPage: Hashable, Codable {
+    var index: Int
     var questionIndices: [QuestionIndex] = []
-    var contents: AttributedString
+    var rawText: String
+    
+    func getPage(pdf: PDFDocument) -> PDFPage {
+        pdf.page(at: index - 1)!
+    }
 }
