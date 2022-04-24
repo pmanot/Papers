cb11f3d570751eeb64cf69146f54f2520e4349f0
Purav Manot <manot.purav@gmail.com>
2022-04-25 00:33:42 +0530

    Remove metadata and contents from Question

--- Shared/Models/Cambridge/Question.swift
+++ Shared/Models/Cambridge/Question.swift
@@ -7,17 +7,17 @@ import PDFKit
 
 struct Question: Hashable, Identifiable {
     let id = UUID()
-    let metadata: CambridgePaperMetadata
+    let details: CambridgePaperDetails
     let index: QuestionIndex
     var check: QuestionCheck = .unsolved
     var pages: [CambridgePaperPage]
-    let contents: AttributedString
+    var rawText: String {
+        pages.map { $0.rawText }.reduce("", +)
+    }
     
     init(index: QuestionIndex, pages: [CambridgePaperPage], metadata: CambridgePaperMetadata){
         self.index = index
         self.pages = pages
-        self.contents = pages.map { $0.contents }.reduce("", +)
-        self.metadata = metadata
         self.details = CambridgePaperDetails(paperCode: metadata.code)
     }
     
