9dc8eb9e1d0bc25b6c767d756cb3656eaf23ac95
Purav Manot <manot.purav@gmail.com>
2022-07-08 22:17:32 +0530

    Refactor

---
+++ Shared/Files/Modifiers/TagTextStyle.swift
@@ -0,0 +1,32 @@
+//
+//  TagTextStyle.swift
+//  Papers
+//
+//  Created by Purav Manot on 05/07/22.
+//
+
+import SwiftUI
+
+struct TagTextStyle: ViewModifier {
+    @Environment(\.colorScheme) var colorScheme
+    var color: Color = .accentColor
+    func body(content: Content) -> some View {
+        switch colorScheme {
+            case .dark:
+                content
+                    .foregroundColor(.primary)
+                    .font(.subheadline)
+                    .padding(7)
+                    .background(color.opacity(0.7).cornerRadius(8))
+                    .shadow(radius: 0.5)
+            default:
+                content
+                    .foregroundColor(.white)
+                    .font(.subheadline)
+                    .padding(7)
+                    .background(color.opacity(0.7).cornerRadius(8))
+                    .shadow(radius: 0.5)
+        }
+        
+    }
+}
